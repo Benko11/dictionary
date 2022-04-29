9dfe622d1d7a86c990b739dfa8f8e3525f7b27d6
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2022-04-29 20:59:03 +0200

    fix

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:3000';
+const URL = 'https://bluom-dictionary.herokuapp.com/';
 
 const wordsEl = document.querySelector('.words');
 const wordItem = document.getElementById('word-item');
